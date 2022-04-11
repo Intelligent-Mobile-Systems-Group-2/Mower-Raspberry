2fade1ed1cfd1f7b46cacef891edc92e0cc3c9b3
Yazan30 <yazan.arab97@hotmail.com>
2022-04-11 23:41:51 +0200

    mowerMovments.py

--- mowerMovments.py
+++ mowerMovments.py
@@ -2,14 +2,21 @@ import serial
 import time
 from picamera import PiCamera
 
-action = b'goForward'
+
+file_name = "/home/pi/Pictures/img_" + str(time.time()) + ".jpg"
+camera = PiCamera()
+action = b"goForward\n"
 
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0',9600,timeout=1)
     ser.reset_input_buffer()
-    
     while True:
         ser.write(action)
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
-        time.sleep(3)
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            coordinates = line.split(",")
+            x = coordinates[0]
+            y = coordinates[1]
+            z = coordinates[2]
+            print(x + "," + y + ","+ z)
+            
