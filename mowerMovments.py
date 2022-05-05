17a8f505fd0c699e0cfcd7500587ed812da7f88c
yazanarab <62600663+Yazan30@users.noreply.github.com>
2022-05-05 12:58:39 +0200

    Add HTTPS requests to send request to the backend
    Co-Authored-By: William <35743604+GreenSoap@users.noreply.github.com>

--- mowerMovments.py
+++ mowerMovments.py
@@ -9,42 +9,6 @@ from codecs import encode
 import requests
 
 
-def sendRequest():
-
-    conn = http.client.HTTPConnection("ims.matteobernardi.fr")
-    dataList = []
-    boundary = 'wL36Yn8afVp8Ag7AmP8qZ0SA4n1v9T'
-    dataList.append(encode('--' + boundary))
-    dataList.append(encode('Content-Disposition: form-data; name=x;'))
-
-    dataList.append(encode('Content-Type: {}'.format('text/plain')))
-    dataList.append(encode(''))
-
-    dataList.append(encode("5"))
-    dataList.append(encode('--' + boundary))
-    dataList.append(encode('Content-Disposition: form-data; name=y;'))
-
-    dataList.append(encode('Content-Type: {}'.format('text/plain')))
-    dataList.append(encode(''))
-
-    dataList.append(encode("53"))
-    dataList.append(encode('--'+boundary+'--'))
-    dataList.append(encode(''))
-    body = b'\r\n'.join(dataList)
-    payload = body
-    headers = {
-      'Accept-Encoding': 'gzip,deflate,sdch',
-      'Accept-Charset': 'UTF-8',
-      'Content-Type': 'application/json',
-      'Content-type': 'multipart/form-data; boundary={}'.format(boundary)
-    }
-    conn.request("POST", "/boundary-collision", payload, headers)
-    res = conn.getresponse()
-    data = res.read()
-    print(data.decode("utf-8"))
-    
-
-
 camera = PiCamera()
 
 right = b"Right\n"
@@ -57,6 +21,8 @@ connection = "connection"
 
 degree = 20066
 random = "random"
+
+obj_detection_interval = 100
 
 api = "http://ims.matteobernardi.fr/boundary-collision/"
 if __name__ == '__main__':
@@ -78,45 +44,56 @@ if __name__ == '__main__':
            # ser.write(left)
         #if  random == "random":
         ser.write(goRandom)
-
+        
         
         
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
-            print(line)
-            coordinates = line.split(",")
-            data = len(coordinates)
+            data = line.split(",")
+            detected = data[0]
+            x = data[1]
+            y = data[2]
             
-            if data == 2:
-                x = coordinates[0]
-                y = coordinates[1]
-                print(x + "," + y)
-                
+            detected_x = 0
+            detected_y = 0
+            
+            # if same coordinates as obj or line detected, skip this iteration
+            if detected_x == x and detected_y == y or detected == "NothingDetected":
+                continue
+            
+            data = len(data)
+            
             if data == 3:
-                if coordinates[0] == "objectDetected":
+                detected_x = x
+                detected_x = y
+                coord_obj = {
+                    "x": x,
+                    "y": y
+                }
+                
+                print(detected, coord_obj)
+                
+                if detected == "objectDetected":
                     camera.capture("/home/pi/Pictures/img.jpg")
-                    image = '/home/pi/Pictures/img.jpg'
-                    detected = coordinates[0]
-                    x = coordinates[1]
-                    y = coordinates[2]
-                    print(detected+ "," + x + "," + y )
-                    imageRequest = requests.post("http://ims.matteobernardi.fr/object-collision/", data={"x": x, "y": y, "photo": image})
-                    try:
-                        print(imageRequest.text)
-                        print(imageRequest.status_code)
-                    except:
-                          print("Error")
-            
-                if coordinates[0] == "lineDetected":
-                    detected = coordinates[0]
-                    x = coordinates[1]
-                    y = coordinates[2]
-                    print(detected + "," + x + "," + y)
+                    image_fd = open('/home/pi/Pictures/img.jpg', 'rb')
+                    image_bin = image_fd.read()
+                    image_fd.close()
+                    image_obj = {
+                        "photo": image_bin
+                    }
                     
-                    lineRequest = requests.post("http://ims.matteobernardi.fr/boundary-collision/", data={"x": x, "y": y})
                     try:
-                        print(lineRequest.text)
-                        print(lineRequest.status_code)
-                    except:
-                          print("Error")
+                        response = requests.post("http://ims.matteobernardi.fr/object-collision", data=coord_obj, files=image_obj)
+                        print(response.text)
+                        print(response.status_code)
+                    except Exception as e:
+                          print("Error", e)
+            
+                elif detected == "lineDetected":
+                    try:
+                        response = requests.post("http://ims.matteobernardi.fr/boundary-collision", json=coord_obj)
+                        print(response.text)
+                        print(response.status_code)
+                    except Exception as e:
+                          print("Error", e)
 
